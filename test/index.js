f2d9797be414aed423add8fa1380b604466d2130
Jordan Harband <ljharb@gmail.com>
2024-06-16 10:36:39 -0700

    [Refactor] reject earlier when the URL is not a string

--- test/index.js
+++ test/index.js
@@ -4,19 +4,46 @@ require('es5-shim');
 require('es6-shim');
 
 var test = require('tape');
-var json = require('../');
+var v = require('es-value-fixtures');
+var inspect = require('object-inspect');
+
+var getJSON = require('../');
+
+var rejects = function assertRejects(fn, expected, msg, extra) {
+	var t = this;
+	/* eslint no-invalid-this: 0 */
+	return new Promise(function (resolve) { resolve(fn()); }).then(
+		function () {
+			throw new SyntaxError('expected promise to reject; it fulfilled');
+		},
+		function (err) {
+			t['throws'](function () { throw err; }, expected, msg, extra);
+		}
+	);
+};
+
+test('rejects on a non-string', function (t) {
+	return Promise.all(v.nonStrings.map(function (nonString) {
+		return t.assertion(
+			rejects,
+			function () { return getJSON(nonString); },
+			TypeError,
+			inspect(nonString) + ' is not a string'
+		);
+	}));
+});
 
 test('Get IP from JSONTest API', function (t) {
 	t.plan(2);
 
-	json('http://ip.jsontest.com/', function (error, body) {
+	getJSON('http://ip.jsontest.com/', function (error, body) {
 		t.error(error);
 		t.ok(body.ip);
 	});
 });
 
 test('Get IP from JSONTest API (with Promise)', function (t) {
-	return json('http://ip.jsontest.com/').then(function (body) {
+	return getJSON('http://ip.jsontest.com/').then(function (body) {
 		t.ok(body.ip);
 	})['catch'](function (error) {
 		t.ok(error);
